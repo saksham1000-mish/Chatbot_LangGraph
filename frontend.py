37b0a78bd951a7c4fb721db8a142629aaeafe5c9
Saksham Mishra <saksham007.mishra@gmail.com>
2025-11-09 20:40:43 +0530

    adding a sidebar

--- frontend.py
+++ frontend.py
@@ -1,11 +1,39 @@
 import streamlit as st
 from backend import chatbot
 from langchain_core.messages import HumanMessage
+import uuid
 
-CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+def generate_thread_id():
+    return uuid.uuid4()
+
+def reset_chat():
+    thread_id = generate_thread_id()
+    st.session_state['thread_id'] = thread_id
+    add_thread(st.session_state['thread_id'])
+    st.session_state['message_history'] = []
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generate_thread_id
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads'] = []
+add_thread(st.session_state['thread_id'])
+
+st.sidebar.title('LangGraph Chatbot')
+if st.sidebar.button('New Chat'):
+    reset_chat()
+st.sidebar.header('My Conversations')
+
+for thread_id in st.session_state['chat_threads']:
+    st.sidebar.text(st.session_state['thread_id'])
+
 
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
@@ -18,6 +46,7 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
     
+    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
     
     
     with st.chat_message('assistant'):
@@ -25,8 +54,10 @@ if user_input:
         ai_message = st.write_stream(
             message_chunk.content for message_chunk, metadata in chatbot.stream(
                 {'messages':[HumanMessage(content = user_input)]},
-                config = {'configurable': {'thread_id': 'thread-1'}},
+                config = CONFIG,
                 stream_mode='messages'
             )
         )
     st.session_state['message_history'].append({'role':'assistant', 'content': ai_message })
+
+
