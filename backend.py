40f163dca39f2886d5b9bf9b089d50ffcce53d8b
Saksham Mishra <saksham007.mishra@gmail.com>
2025-11-10 22:59:04 +0530

    adding sqlite db

--- backend.py
+++ backend.py
@@ -2,9 +2,10 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
@@ -18,7 +19,8 @@ def chat_node(state: ChatState):
     response = model.invoke(messages)
     return {'messages': [response]}
 
-checkpointer = InMemorySaver()
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+checkpointer = SqliteSaver(conn = conn)
 
 graph = StateGraph(ChatState)
 
@@ -29,3 +31,10 @@ graph.add_edge('chat_node', END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
--- frontend.py
+++ frontend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from backend import chatbot
+from backend import chatbot, retrieve_all_threads
 from langchain_core.messages import HumanMessage
 import uuid
 
@@ -27,7 +27,7 @@ if 'thread_id' not in st.session_state:
     st.session_state['thread_id'] = generate_thread_id
 
 if 'chat_threads' not in st.session_state:
-    st.session_state['chat_threads'] = []
+    st.session_state['chat_threads'] = retrieve_all_threads()
 add_thread(st.session_state['thread_id'])
 
 st.sidebar.title('LangGraph Chatbot')
